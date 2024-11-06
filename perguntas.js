5083b7194a1c78cce1cd730d12d225ba15ac14aa
MariaPaulinoTecnologia <00001117755460SP@al.educacao.sp.gov.br>
2024-11-06 08:32:21 -0300

    .

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'KPOP',
+    'Qual maior grupo de kpop?',
+    'BTS'
+)
+
+criaCartao(
+    'BIBLIA',
+    'Quem abriu o mar vermelho?',
+    'Moises'
+)
+
+criaCartao(
+    'Eporte',
+    'qual a data da morte de Ayrton senna?',
+    '1 de maio de 1994'
+)
+
+criaCartao(
+    'Anime ',
+    'Quem é o jovem pirata que conquistou geral?',
+    'luffy'
+)
